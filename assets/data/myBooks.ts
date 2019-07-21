f84f9b5af73929b54fd8279b6d1bd7aa110e8cf9
gewenf <52514357+gewenf@users.noreply.github.com>
2019-07-21 01:31:57 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,10 @@
+import {BookInfo} from '../../app/bookInfo';
+
+export const MYBOOKS: BookInfo[]=
+[
+    {bookname:'Journey to the West', author:"Wu,Cheng'en", genre:'Mystery', year:'1592', picture:"assets/images/001.jpg"},
+    {bookname:'Ode to Gallantry', author:'Jin, Yong', genre:'Martial Heroes', year:'1965', picture:"assets/images/002.jpg"},
+    {bookname:'Records of the Grand Historian', author:'Sima, Qian', genre:'History', year:'91BC', picture:"assets/images/003.jpg"},
+    {bookname:'300 Song Poems', author:'Zhu, Xiaozang', genre:'Poems', year:'1924', picture:"assets/images/004.jpg"}
+ 
+]
